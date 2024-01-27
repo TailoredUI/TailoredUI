f846c84ce6202c8bfca42a11db2221af61624afc
chandel-aman <chandel.s.aman@gmail.com>
2024-01-27 20:43:04 +0530

    removed the unused declaration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import FeatureSection from "../components/Home/FeatureSection";
 import Footer from "../components/Home/FooterSection";
 import HeroSection from "../components/Home/HeroSection";
-import StaticComponent from "../components/Home/StaticComponent";
 
 const Home = () => {
   return (
     <main>
       <HeroSection />
-      {/* <StaticComponent /> */}
       <FeatureSection />
       <Footer />
     </main>
