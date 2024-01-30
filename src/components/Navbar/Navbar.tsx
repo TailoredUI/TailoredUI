ef3a5582d3f8f0a6ea8fea48be415b3fbf709e84
chandel-aman <chandel.s.aman@gmail.com>
2024-01-30 15:03:38 +0530

    added tailoredui icon and favicon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Modal from 'react-modal'
 import { easeOut, motion } from 'framer-motion'
 import { componentsList } from '../../utils/constant/values'
 import { Component } from '../../utils/types/types'
+import TailoredUIIcon from '../../assets/images/icons/TailoredUI.icon'
 
 const Navbar = () => {
   const [isSearchVisible, setIsSearchVisible] = useState(false)
@@ -49,21 +50,13 @@ const Navbar = () => {
       <nav className="relative z-10 m-auto my-4 w-[90%] rounded-lg bg-gradient-to-r from-slate-600 to-transparent p-4 backdrop-blur-sm md:w-[95%] xl:w-[75%]">
         <div className="container mx-auto flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="#FFF"
-              className="h-6 w-6"
+            <motion.div
+              initial={{ scale: 0, opacity: 0, rotate: -270 }}
+              animate={{ scale: 1, opacity: 1, rotate: 0 }}
+              transition={{ duration: 1.2, ease: easeOut }}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42"
-              />
-            </svg>
-
+              <TailoredUIIcon className="h-10 w-10" />
+            </motion.div>
             <span className="text-lg font-bold text-white">TailoredUI</span>
           </div>
 
