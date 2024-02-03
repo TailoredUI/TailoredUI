97183fa82b623611a06c71f59595b60e4e4ba3d4
chandel-aman <chandel.s.aman@gmail.com>
2024-02-03 23:36:09 +0530

    added types

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,6 +1,7 @@
 // types.ts
 export const SELECT_COMPONENT = 'SELECT_COMPONENT' as const
 export const SELECTED_COMPONENT_CONFIG = 'SELECT_COMPONENT_CONFIG' as const
+export const SELECTED_COLLECTION = 'SELECTED_COLLECTION' as const
 
 // Action types
 export type SelectComponentAction = {
@@ -11,4 +12,9 @@ export type SelectComponentAction = {
 export type SelectedComponentConfigAction = {
   type: typeof SELECTED_COMPONENT_CONFIG
   payload: unknown
+}
+
+export type SelectedCollectionAction = {
+  type: typeof SELECTED_COLLECTION
+  payload: string
 }
