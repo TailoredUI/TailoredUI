62333ec20ab46672ab4c85929e1c549fa555ac4b
Rakshit Sisodiya <64471316+Rakshit-gen@users.noreply.github.com>
2024-05-09 21:16:37 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -95,6 +95,7 @@ const Navbar = () => {
 
             {/* Github icon */}
             <span className="hidden xl:inline-flex">
+              <a href='https://github.com/TailoredUI/TailoredUI'>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -110,6 +111,7 @@ const Navbar = () => {
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
               </svg>
+                </a>
             </span>
 
             <button className="ml-4 hidden rounded-md bg-blue-500 px-8 py-2 text-white md:inline-block">
