8e9fa4cbaf21bdf328c3844bf32b9849429c6309
PranavKeshav24 <127531721+PranavKeshav24@users.noreply.github.com>
2024-05-11 22:18:18 +0530

    Reordered classnames again to fix lint error

--- src/components/Navbar/MenuOverlay.tsx
+++ src/components/Navbar/MenuOverlay.tsx
@@ -44,7 +44,7 @@ const MenuOverlay: React.FC<MenuOverlayProps> = ({
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.4 }}
-            className="absolute left-0 right-0 mx-auto grid text-center p-4 gap-y-2 rounded-lg backdrop-blur-sm z-10 w-[90%] md:w-[95%] xl:w-[75%] grid-rows-3 md:hidden bg-gradient-to-r from-slate-600 to-transparent"
+            className="absolute left-0 right-0 mx-auto p-4 grid text-center gap-y-2 rounded-lg backdrop-blur-sm z-10 w-[90%] md:w-[95%] xl:w-[75%] grid-rows-3 md:hidden bg-gradient-to-r from-slate-600 to-transparent"
           >
             {/* Map through links to create navigation items */}
             {links.map((link, index) => (
