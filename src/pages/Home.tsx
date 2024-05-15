fe214b0a5c963af5df66a5de29c44858445533c5
PranavKeshav24 <127531721+PranavKeshav24@users.noreply.github.com>
2024-05-15 21:32:56 +0530

    Fixed issues related to page breaking on multiple screen sizes.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import HeroSection from '../components/Home/HeroSection'
 
 const Home = () => {
   return (
-    <div className="flex flex-col md:gap-y-6">
+    <div className="flex flex-col md:gap-16 lg:gap-y-0 md:py-16 lg:py-0">
       <HeroSection />
       <FeatureSection />
     </div>
