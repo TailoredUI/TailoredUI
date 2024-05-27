aaa588ce57a479a136def6a8e0b8e14a4d92d59b
PranavKeshav24 <127531721+PranavKeshav24@users.noreply.github.com>
2024-05-27 21:44:53 +0530

    Lint errors resolved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import HeroSection from '../components/Home/HeroSection'
 
 const Home = () => {
   return (
-    <div className="flex flex-col md:py-16 lg:py-0 md:gap-16 lg:gap-y-0">
+    <div className="flex flex-col md:gap-16 md:py-16 lg:gap-y-0 lg:py-0">
       <HeroSection />
       <FeatureSection />
     </div>
